2bb191f64027f55325daffb931aea9b8725408bf
mariaShcherbak <maria.shcherbakk@gmail.com>
2021-11-02 10:30:54 +0200

    mnbvbnm,

--- Weather/API/servises/NetworkService.swift
+++ Weather/API/servises/NetworkService.swift
@@ -8,16 +8,21 @@
 import Foundation
 
 protocol NetworkServiceProtocol {
-    func getCity(completition: @escaping(Result<[CurrentWeather]?, Error>) -> Void)
+    func getCity()
 }
 
+
 class NetworkService: NetworkServiceProtocol {
-    func getCity(completition: @escaping (Result<[CurrentWeather]?, Error>) -> Void) {
-        
-        let urlString = "http://htmlweb.ru/geo/api.php?json&city_name=Харьков&api_key=38db0239405b5a7dcea6c9890f99ccb9"
-        
-        
-        
+    func getCity() {
+       var searchText = "Khar"
+        var urll = "http://htmlweb.ru/geo/api.php?json&city_name=" + searchText + "&38db0239405b5a7dcea6c9890f99ccb9"
+        var request = URLRequest(url: URL(string: urll)!)
+        var task = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            print(String(decoding: data!, as: UTF8.self))
+            print(error)
+        }
+        task.resume()
         
         
         
@@ -26,4 +31,5 @@ class NetworkService: NetworkServiceProtocol {
         
         //API_key: 38db0239405b5a7dcea6c9890f99ccb9
     }
+    
 }
