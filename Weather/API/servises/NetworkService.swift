f4b7d72dd365dfaab8e025ab6b9a1973be37ba12
mariaShcherbak <maria.shcherbakk@gmail.com>
2021-11-06 22:54:21 +0200

    лор

--- Weather/API/servises/NetworkService.swift
+++ Weather/API/servises/NetworkService.swift
@@ -15,18 +15,22 @@ protocol NetworkServiceProtocol {
 class NetworkService: NetworkServiceProtocol {
     var cityArray: [City]?
     func getCity(string: String, completition: @escaping([City]) -> Void) {
-        var searchText = "Kharkov"
-        var url = "http://htmlweb.ru/geo/api.php?json&city_name=" + searchText + "&38db0239405b5a7dcea6c9890f99ccb9"
+        var searchText = "харьков"
+        var url = "http://htmlweb.ru/geo/api.php?city_name=" + searchText + "&json&api_key=38db0239405b5a7dcea6c9890f99ccb9"
+        print(url)
+        /////http://htmlweb.ru/geo/api.php?city_name=Kharkov&json&api_key=38db0239405b5a7dcea6c9890f99ccb9
         var request = URLRequest(url: URL(string: url)!)
         var task = URLSession.shared.dataTask(with: request) {
             data, response, error in
             if let data = data, let city = try? JSONDecoder().decode([City].self, from: data) {
-                print("атппапталп")
                 completition(city)
                 print(completition(city))
                 self.cityArray = city
-                print(self.cityArray)
+              //  print(self.cityArray)
               //  cityArray?.append([City])
+            }
+            else {
+                
             }
         }
         task.resume()
